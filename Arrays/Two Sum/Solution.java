30c5fa503c4f38e74ccbc8b8a3625b911241e0a9
ANMOL PANT <48082902+Anmol1109@users.noreply.github.com>
2026-03-22 10:59:06 +0530

    Add files via upload

--- Arrays/Two Sum/Solution.java
+++ Arrays/Two Sum/Solution.java
@@ -1,13 +1,34 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> numToIndex = new HashMap<>();
+  public List<List<Integer>> threeSum(int[] nums) {
+    if (nums.length < 3)
+      return new ArrayList<>();
 
-    for (int i = 0; i < nums.length; ++i) {
-      if (numToIndex.containsKey(target - nums[i]))
-        return new int[] {numToIndex.get(target - nums[i]), i};
-      numToIndex.put(nums[i], i);
+    List<List<Integer>> ans = new ArrayList<>();
+
+    Arrays.sort(nums);
+
+    for (int i = 0; i + 2 < nums.length; ++i) {
+      if (i > 0 && nums[i] == nums[i - 1])
+        continue;
+
+      int l = i + 1;
+      int r = nums.length - 1;
+      while (l < r) {
+        final int sum = nums[i] + nums[l] + nums[r];
+        if (sum == 0) {
+          ans.add(Arrays.asList(nums[i], nums[l++], nums[r--]));
+          while (l < r && nums[l] == nums[l - 1])
+            ++l;
+          while (l < r && nums[r] == nums[r + 1])
+            --r;
+        } else if (sum < 0) {
+          ++l;
+        } else {
+          --r;
+        }
+      }
     }
 
-    throw new IllegalArgumentException();
-    }
+    return ans;
+  }
 }
